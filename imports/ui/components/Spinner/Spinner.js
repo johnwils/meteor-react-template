cffeb727bee2c7544749260adb97683a15c51bf2
John Wilson <johnner@me.com>
2018-02-09 17:28:52 -0800

    add Spinner component

---
+++ imports/ui/components/Spinner/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Spinner.scss';
+
+const Spinner = () => (
+  <div className="login-spinner">
+    <div>
+      <i className="fa fa-circle-o-notch fa-spin" />
+    </div>
+  </div>
+);
+
+export default Spinner;
