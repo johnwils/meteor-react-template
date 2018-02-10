e4632b938d8fee9c23b31596de7e3d055621d0b1
John Wilson <johnner@me.com>
2018-02-09 17:40:23 -0800

    add spinning to App layout
    - improve prop validation
    - fix eslint warnings
    - run prettier

--- imports/ui/layouts/App.js
+++ imports/ui/layouts/App.js
@@ -2,8 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { render } from 'react-dom';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // import navbar
 import Navbar from '../../ui/components/Navbar';
@@ -18,20 +17,24 @@ import Profile from '../../ui/pages/Profile';
 import RecoverPassword from '../../ui/pages/RecoverPassword';
 import NotFound from '../../ui/pages/Not-Found';
 
-const App = appProps => (
+// import Spinner
+import Spinner from '../components/Spinner';
+
+const App = props => (
   <Router>
     <div>
-      <Navbar {...appProps} />
+      <Navbar authenticated={props.authenticated} user={props.user} />
+      {props.loggingIn && <Spinner />}
       <div className="container-fluid">
         <Switch>
-          <Public path="/login" component={Login} {...appProps} />
+          <Public path="/login" component={Login} {...props} />
           <Route exact name="landing" path="/" component={Landing} />
-          <Public path="/signup" component={Signup} {...appProps} />
-          <Authenticated exact path="/profile" component={Profile} {...appProps} />
+          <Public path="/signup" component={Signup} {...props} />
+          <Authenticated exact path="/profile" component={Profile} {...props} />
           <Route name="recover-password" path="/recover-password" component={RecoverPassword} />
           {/*
           TODO: add remaining routes
-          <Authenticated exact path="/profile/:_id" component={Profile} {...appProps} />
+          <Authenticated exact path="/profile/:_id" component={Profile} {...props} />
           <Route name="reset-password" path="/reset-password/:token" component={ResetPassword} />
           */}
           <Route component={NotFound} />
@@ -41,18 +44,21 @@ const App = appProps => (
   </Router>
 );
 
-App.propTypes = {
-  loggingIn: PropTypes.bool,
-  authenticated: PropTypes.bool,
-  userId: PropTypes.string,
+App.defaultProps = {
+  userId: null,
+  user: null,
 };
 
-export default withTracker(() => {
-  const loggingIn = Meteor.loggingIn();
-  return {
-    loggingIn,
-    authenticated: !loggingIn && !!Meteor.userId(),
-    userId: Meteor.userId(),
-    user: Meteor.user(),
-  };
-})(App);
+App.propTypes = {
+  loggingIn: PropTypes.bool.isRequired,
+  authenticated: PropTypes.bool.isRequired,
+  userId: Meteor.user() ? PropTypes.string.isRequired : () => null,
+  user: Meteor.user() ? PropTypes.object.isRequired : () => null,
+};
+
+export default withTracker(() => ({
+  loggingIn: Meteor.loggingIn(),
+  authenticated: Boolean(!Meteor.loggingIn() && !!Meteor.userId()),
+  userId: Meteor.userId(),
+  user: Meteor.user(),
+}))(App);
