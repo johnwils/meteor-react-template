72b561af15e53136b4850e5861cd08a5a7490ada
John Wilson <johnner@me.com>
2018-02-09 17:30:57 -0800

    add remote ddp Users example

--- imports/ui/pages/Profile/Profile.js
+++ imports/ui/pages/Profile/Profile.js
@@ -2,41 +2,82 @@ import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Counters } from '../../../api/counters/counters';
+
+// collection
+import Counters from '../../../api/counters/counters';
+
+// remote example (if using ddp)
+/*
+import Remote from '../../../api/remote/ddp';
+import Users from '../../../api/remote/users';
+*/
 
 // components
 import Modal, { Button } from '../../components/Modal/Modal';
-import { default as AddCountButton } from '../../components/Button';
+import AddCountButton from '../../components/Button';
 import Text from '../../components/Text';
 
 import './Profile.scss';
 
-const Profile = ({ userId, counterExists, counter, subReady }) => {
+const Profile = ({
+  userId,
+  // remote example (if using ddp)
+  // usersReady,
+  // users,
+  countersReady,
+  counter,
+}) => { // eslint-disable-line
+  // remote example (if using ddp)
+  /*
+  console.log('usersReady', usersReady);
+  console.log('users', users);
+  */
   return (
     <div className="profile-page">
       <h1>Profile Page</h1>
       <Button target="userId" type="primary" title="Click for User Info" />
-      <Modal target="userId" title="User Info" body={userId} counter={counter} />
-      <hr/>
-      {counterExists && <Text count={counter.count} />}
+      {countersReady && <Modal target="userId" title="User Info" body={userId} counter={counter} />}
+      <hr />
+      {countersReady && <Text count={counter.count} />}
       <AddCountButton />
     </div>
   );
 };
 
+Profile.defaultProps = {
+  // users: null, remote example (if using ddp)
+  counter: null,
+};
+
 Profile.propTypes = {
-  counter: PropTypes.object,
-  counterExists: PropTypes.bool.isRequired,
   userId: PropTypes.string.isRequired,
+  // remote example (if using ddp)
+  // usersReady: PropTypes.bool.isRequired,
+  // users: Meteor.user() ? PropTypes.array.isRequired : () => null,
+  countersReady: PropTypes.bool.isRequired,
+  counter: Meteor.user()
+    ? PropTypes.shape({
+      _id: PropTypes.string,
+      count: PropTypes.number,
+    }).isRequired
+    : () => null,
 };
 
 export default withTracker(({ userId }) => {
-  const countersHandle = Meteor.subscribe('counters.all');
-  const loading = !countersHandle.ready();
-  const counter = Counters.findOne(userId);
-  const counterExists = !loading && !!counter;
+  // remote example (if using ddp)
+  /*
+  const usersSub = Remote.subscribe('users.all'); // publication needs to be set on remote server
+  const usersReady = usersSub.ready();
+  */
+
+  // counters example
+  const countersSub = Meteor.subscribe('counters.all');
+  const countersReady = countersSub.ready();
   return {
-    counter,
-    counterExists,
+    // remote example (if using ddp)
+    // usersReady,
+    // users: usersReady ? Users.find().fetch() : null,
+    countersReady,
+    counter: countersReady ? Counters.findOne(userId) : null,
   };
 })(Profile);
