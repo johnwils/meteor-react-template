31d7c2860cfc7494b4f61804f1647345bb883dd1
John Wilson <johnner@me.com>
2018-02-17 17:41:06 -0800

    remove common / server methods
    client/server code is separated using validated methods

--- imports/api/counters/methods.common.tests.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-// Tests for common methods
-//
-// https://guide.meteor.com/testing.html
-
-// import { Meteor } from 'meteor/meteor';
-// import { assert } from 'meteor/practicalmeteor:chai';
-// import Counters from './counters.js';
-// import { countersIncrease } from './methods.common.js';
