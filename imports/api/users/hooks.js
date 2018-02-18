fce9f9dc43389ab0cee1a7f36a85121481ec5a06
John Wilson <johnner@me.com>
2018-02-17 17:45:51 -0800

    add 'user' and 'admin' role hooks
    first user to sign up is 'admin', then 'user'

---
+++ imports/api/users/hooks.js
@@ -0,0 +1,15 @@
+/**
+ * Collection Hooks
+ */
+
+import { Meteor } from 'meteor/meteor';
+import { Roles } from 'meteor/alanning:roles';
+
+Meteor.users.after.insert(function (userId, doc) {
+  if (Meteor.users.find().count() === 0) {
+    console.log("new admin registered, added to 'admin' role", doc._id);
+    return Roles.addUsersToRoles(doc._id, ['admin'], Roles.GLOBAL_GROUP);
+  }
+  console.log("new user registered, added to 'user' role", doc._id);
+  return Roles.addUsersToRoles(doc._id, ['user'], Roles.GLOBAL_GROUP);
+});
