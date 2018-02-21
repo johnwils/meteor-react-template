484d58fc47f2473cf7c9ee277a98f4da144b6b58
John Wilson <johnner@me.com>
2018-02-21 12:26:32 -0800

    fix logs

--- imports/api/users/hooks.js
+++ imports/api/users/hooks.js
@@ -5,11 +5,13 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 
-Meteor.users.after.insert(function (userId, doc) {
-  if (Meteor.users.find().count() === 0) {
+Meteor.users.after.insert(function(userId, doc) {
+  if (!userId && Meteor.users.find().count() === 0) {
     console.log("new admin registered, added to 'admin' role", doc._id);
     return Roles.addUsersToRoles(doc._id, ['admin'], Roles.GLOBAL_GROUP);
   }
-  console.log("new user registered, added to 'user' role", doc._id);
-  return Roles.addUsersToRoles(doc._id, ['user'], Roles.GLOBAL_GROUP);
+  if (!userId) {
+    console.log("new user registered, added to 'user' role", doc._id);
+    return Roles.addUsersToRoles(doc._id, ['user'], Roles.GLOBAL_GROUP);
+  }
 });
