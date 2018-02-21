ff703225dbda3874e5a927d5fa4cc62714406e53
John Wilson <johnner@me.com>
2018-02-21 09:39:42 -0800

    add routes, clean up

--- imports/ui/layouts/App.js
+++ imports/ui/layouts/App.js
@@ -1,63 +1,64 @@
+/* eslint-disable import/no-named-default */
+
+// import packages
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
 // import navbar
 import Navbar from '../../ui/components/Navbar';
 
 // import routes
-import PropsRoute from '../../ui/pages/PropsRoute';
 import Landing from '../../ui/pages/Landing';
 import Login from '../../ui/pages/Login';
 import Signup from '../../ui/pages/Signup';
 import Profile from '../../ui/pages/Profile';
-import RecoverPassword from '../../ui/pages/RecoverPassword';
 import NotFound from '../../ui/pages/Not-Found';
+import RecoverPassword from '../../ui/pages/RecoverPassword';
+import ResetPassword from '../../ui/pages/ResetPassword';
 
 // import Spinner
 import Spinner from '../components/Spinner';
 
+// import hoc to pass additional props to routes
+import PropsRoute from '../pages/PropsRoute';
+
 const App = props => (
   <Router>
     <div>
-      <Navbar authenticated={props.authenticated} user={props.user} />
+      <PropsRoute component={Navbar} {...props} />
       {props.loggingIn && <Spinner />}
-      <div className="container-fluid">
-        <Switch>
-          <PropsRoute exact name="landing" path="/" component={Landing} {...props} />
-          <PropsRoute exact path="/login" component={Login} {...props} />
-          <PropsRoute exact path="/signup" component={Signup} {...props} />
-          <PropsRoute exact path="/profile" component={Profile} {...props} />
-          <PropsRoute exact path="/recover-password" component={RecoverPassword} {...props} />
-          {/*
-          TODO: add remaining routes
-          <Authenticated exact path="/profile/:_id" component={Profile} {...props} />
-          <Route name="reset-password" path="/reset-password/:token" component={ResetPassword} />
-          */}
-          <Route component={NotFound} />
-        </Switch>
-      </div>
+      <Switch>
+        <PropsRoute exact path="/" component={Landing} {...props} />
+        <PropsRoute path="/login" component={Login} {...props} />
+        <PropsRoute path="/signup" component={Signup} {...props} />
+        <PropsRoute exact path="/profile" component={Profile} {...props} />
+        <PropsRoute exact path="/profile/:_id" component={Profile} {...props} />
+        <PropsRoute path="/recover-password" component={RecoverPassword} {...props} />
+        <PropsRoute path="/reset-password/:token" component={ResetPassword} {...props} />
+        <PropsRoute component={NotFound} {...props} />
+      </Switch>
     </div>
   </Router>
 );
 
-App.defaultProps = {
-  userId: null,
-  user: null,
-};
-
 App.propTypes = {
   loggingIn: PropTypes.bool.isRequired,
-  authenticated: PropTypes.bool.isRequired,
-  userId: Meteor.user() ? PropTypes.string.isRequired : () => null,
-  user: Meteor.user() ? PropTypes.object.isRequired : () => null,
+  userReady: PropTypes.bool.isRequired,
+  loggedIn: PropTypes.bool.isRequired,
 };
 
-export default withTracker(() => ({
-  loggingIn: Meteor.loggingIn(),
-  authenticated: Boolean(!Meteor.loggingIn() && !!Meteor.userId()),
-  userId: Meteor.userId(),
-  user: Meteor.user(),
-}))(App);
+export default withTracker(() => {
+  const userSub = Meteor.subscribe('user');
+  const user = Meteor.user();
+  const userReady = userSub.ready() && !!user;
+  const loggingIn = Meteor.loggingIn();
+  const loggedIn = !loggingIn && userReady;
+  return {
+    loggingIn,
+    userReady,
+    loggedIn,
+  };
+})(App);
